8b74e31157f3a0fcff74cdad9046ab1ec0cce803
Sean Kirkpatrick <Skirkp18@gmail.com>
2020-09-16 21:01:09 -0400

    fixed api call

--- script.js
+++ script.js
@@ -9,42 +9,14 @@ function getCocktail () {
     }).then(function(r) {
         console.log(r);
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-
-
-
-
-// DRINK API CALL
-// get drink 
-function getCocktail () {
-    var alcohol = "vodka"
-    var alQueryURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + alcohol;
-    $.ajax({
-        url: alQueryURL,
-        method: "GET"
-    }).then(function(r) {
-        console.log(r);
-
->>>>>>> a116d7be252f08f0d6ebd3329bec0a01a266d662
-
-
-
-
     });  
 
 };
 
-<<<<<<< HEAD
 
-// RECIPE API CALL
-// get recipes
-=======
-getCocktail();
-=======
->>>>>>> a116d7be252f08f0d6ebd3329bec0a01a266d662
+
+
+
 function edamamRecipieAPICall() {
 
 var edamamID = "9d7a8164";
@@ -59,11 +31,8 @@ $.ajax({
   console.log(response);
 });
 };
-<<<<<<< HEAD
+
 
 getCocktail();
 edamamRecipieAPICall();
 
-=======
->>>>>>> master
->>>>>>> a116d7be252f08f0d6ebd3329bec0a01a266d662
