b1f4ad3c0f43842a04a13c83e83d26c9c4f4a7ad
Sean Kirkpatrick <Skirkp18@gmail.com>
2020-09-16 20:42:26 -0400

    fixed edamam API call

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-// edamam api function
+// DRINK API CALL
+// get drink 
+function getCocktail () {
+    var alcohol = "vodka"
+    var alQueryURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + alcohol;
+    $.ajax({
+        url: alQueryURL,
+        method: "GET"
+    }).then(function(r) {
+        console.log(r);
+
+
+
+
+
+    });  
+
+};
+
+
+
 function edamamRecipieAPICall() {
 
 var edamamID = "9d7a8164";
 var key = "4ce6ec1091b11815141f2432df876863";	
 
-var queryURL = "https://api.edamam.com/search?q=spaghetti&app_id=9320ecc7&app_key=68c58b49df411be74f1ba681a92f0501";
+var queryURL = "https://api.edamam.com/search?q=burger&app_id=" + edamamID + "&app_key=" + key;
 
 $.ajax({
   url: queryURL,
@@ -13,3 +33,6 @@ $.ajax({
   console.log(response);
 });
 };
+
+getCocktail();
+edamamRecipieAPICall();
